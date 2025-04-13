62af8ef0406240ac450f686bc8dd1112ffe8b828
Wonhee Jang <Qtsky89@gmail.com>
2025-04-13 23:15:59 +0900

    https://mikro-orm.io/docs/guide/relationships#-checkpoint-2 done (chapter 2)

--- blog-api/src/modules/user/user.entity.ts
+++ blog-api/src/modules/user/user.entity.ts
@@ -1,8 +1,10 @@
-import { Collection, Entity, OneToMany, Property } from "@mikro-orm/core";
+import { BeforeCreate, BeforeUpdate, Collection, Entity, EventArgs, OneToMany, Property } from "@mikro-orm/core";
 import { BaseEntity } from "../common/base.entity.js";
 // import { t } from '@mikro-orm/core'; // `t` or `types`
 import crypto from 'crypto';
 import { Article } from "./article.entity.js";
+
+import {hash, verify} from 'argon2'
 
 @Entity()
 export class User extends BaseEntity<'bio'>{
@@ -25,11 +27,21 @@ export class User extends BaseEntity<'bio'>{
     super();
     this.fullName = fullName;
     this.email = email;
-    this.password = User.hashPassword(password);
+    this.password = password
   }
 
-  static hashPassword(password: string) {
-    return crypto.createHmac('sha256', password).digest('hex')
+  @BeforeCreate()
+  @BeforeUpdate()
+  async hashPassword(args: EventArgs<User>) {
+    const password = args.changeSet?.payload.password
+    
+    if (password) {
+      this.password = await hash(password)
+    }
+  }
+
+  async verifyPassword(password: string) {
+    return verify(this.password, password);
   }
 
   // @Property({ type: t.text })

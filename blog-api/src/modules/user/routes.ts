c2f1338560d1695302cd90b1e1c36ead266607d4
Wonhee Jang <Qtsky89@gmail.com>
2025-04-16 19:29:37 +0900

    https://mikro-orm.io/docs/guide/advanced#sign-up-route done

--- blog-api/src/modules/user/routes.ts
+++ blog-api/src/modules/user/routes.ts
@@ -1,6 +1,27 @@
 import { FastifyInstance } from 'fastify';
 import { initORM } from '../../db.js';
+import { EntityData } from '@mikro-orm/core';
+import { User } from './user.entity.js';
 
 export async function registerUserRoutes(app: FastifyInstance) {
-  // no routes yet
+  const db = await initORM()
+
+  app.post('/sign-up', async (request) => {
+    const body = request.body as EntityData<User>
+
+    if(!body.email || !body.fullName || !body.password) {
+      throw new Error("One or required field is missing. email, fullName, password")
+    }
+    if (await db.user.count({email: body.email}) > 0 ) {
+      throw new Error("This email is already registered, maybe you want to sign in?")
+    }
+
+    const user = new User(body.fullName, body.email, body.password)
+    user.bio = body.bio ?? '';
+    await db.em.persist(user).flush()
+
+    console.log(`User ${user.id} created`)
+
+    return user
+  })
 }

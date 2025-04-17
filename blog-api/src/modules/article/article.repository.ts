7d90b00a2fbca2f5688535aa72cd1dfa933cd227
Wonhee Jang <Qtsky89@gmail.com>
2025-04-17 12:00:41 +0900

    https://mikro-orm.io/docs/guide/advanced#using-querybuilder done

--- blog-api/src/modules/article/article.repository.ts
+++ blog-api/src/modules/article/article.repository.ts
@@ -4,7 +4,24 @@ import { ArticleListing } from './article-listing.entity.js'
 
 export class ArticleRepository extends EntityRepository<Article> {
   listArticlesQuery() {
+
+    const totalComments = this.em.createQueryBuilder(Comment)
+    .count()
+    .where({article: sql.ref('a.id')})
+    .as('totalComments')
+
+    const usedTags = this.em.createQueryBuilder(Article, 'aa')
+    .select(sql`group_concat(distinct t.name)`)
+    .join('aa.tags', 't')
+    .where({ 'aa.id': sql.ref('a.id') })
+    .groupBy('aa.author')
+    .as('tags');
+
     return this.createQueryBuilder('a')
+    .select(['slug', 'title', 'description', 'author'])
+    .addSelect(sql.ref('u.full_name').as('authorName'))
+    .join('author', 'u')
+    .addSelect([totalComments, usedTags]);
   }
 
   async listArticles(options: FindOptions<ArticleListing>) {

13dc4698758222e822f668bcb8a6e3c10416a478
Wonhee Jang <Qtsky89@gmail.com>
2025-04-17 11:21:08 +0900

    https://mikro-orm.io/docs/guide/advanced#creating-entities done

--- blog-api/src/modules/article/routes.ts
+++ blog-api/src/modules/article/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import {initORM} from '../../db.js'
+import { getUserFromToken } from '../common/utils.js'
 
 export async function registerArticleRoutes(app: FastifyInstance) {
   const db = await initORM()
@@ -11,5 +12,45 @@ export async function registerArticleRoutes(app: FastifyInstance) {
     })
 
     return {items, total}
+  })
+  
+  app.get('/:slug', async (request) =>  {
+    const { slug } = request.params as { slug: string};
+
+    return db.article.findOneOrFail({slug}, {
+      populate: ['author', 'comments.author', 'text'],
+    })
+  })
+
+  app.post('/:slug/comment', async request => {
+    const { slug, text } = request.params as { slug: string; text: string };
+    const author = getUserFromToken(request);
+    const article = await db.article.findOneOrFail({ slug });
+    const comment = db.comment.create({ author, article, text });
+
+  // We can add the comment to `article.comments` collection,
+  // but in fact it is a no-op, as it will be automatically
+  // propagated by setting Comment.author property.
+  // article.comments.add(comment);
+
+    // mention we don't need to persist anything explicitly
+    await db.em.flush();
+
+    return comment;
+  })
+
+  app.post('/', async (request) => {
+    const {title, description, text} = request.body as { title: string, description: string, text: string}
+
+    const author = getUserFromToken(request)
+    const article = db.article.create({
+      title,
+      description,
+      text,
+      author
+    })
+
+    await db.em.flush()
+    return article
   })
 }
